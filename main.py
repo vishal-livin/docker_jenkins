3e3f6765ba7bbb42c820546d5ae4e0ef6bd42db7
vishal-livin <96013911+vishal-livin@users.noreply.github.com>
2022-11-27 15:02:36 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# This program adds two numbers
+
+
+print('hellow world')
+
+print("adding two no program")
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
